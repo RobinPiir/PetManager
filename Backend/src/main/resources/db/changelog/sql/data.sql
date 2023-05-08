3306700c269aa0fc6440ebbf1e365dda2dd05474
Robin Piir <93199438+RobinPiir@users.noreply.github.com>
2023-05-08 23:11:40 +0300

    Fixed typo in data.sql

--- Backend/src/main/resources/db/changelog/sql/data.sql
+++ Backend/src/main/resources/db/changelog/sql/data.sql
@@ -4,7 +4,7 @@ CREATE TABLE pets(id IDENTITY NOT NULL,
     type varchar(255),
     color varchar(255),
     primary key (id));
-INSERT INTO PETS(code,name,type,color) VALUES ('202211151535','Mustikas','Cat','Black'),('202211151536','Roy', 'Dog', 'Brown'),('202211151536','Charles', 'Horse', 'White')('202305081840','Caesar', 'Cat', 'Multicolor');
+INSERT INTO PETS(code,name,type,color) VALUES ('202211151535','Mustikas','Cat','Black'),('202211151536','Roy', 'Dog', 'Brown'),('202211151536','Charles', 'Horse', 'White'),('202305081840','Caesar', 'Cat', 'Multicolor');
 CREATE TABLE colors(id IDENTITY NOT NULL,
 color varchar(255),
 primary key (id));
